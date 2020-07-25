88ce3a136ed3e0f8385f1f5ca31d240164251c9e
unknown <acano3@ucol.mx>
2020-07-25 16:29:37 -0500

    '25/Jul/2020'

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+//const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const app = express();
 
 //////////bdd//////////
 mongoose.connect("mongodb+srv://MrCano369:acr3945@mrcano369-sbz3s.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 var Schema = mongoose.Schema;
-var Esquema = new Schema({
+var esquemaUsuario = new Schema({
     nombre: String,
     correo: String,
-    clave: String
+    clave: String,
+    img: String,
+    coins: Number,
+    exp: Number,
+    active: Boolean
 });
-var Usuario = mongoose.model('Usuario', Esquema);
+var Usuario = mongoose.model('Usuario', esquemaUsuario);
 
 //////////settings//////////
 app.set('view engine', 'ejs');
@@ -22,17 +29,39 @@ app.set('view engine', 'ejs');
 
 //app.use(morgan('dev'));
 app.use('', express.static(__dirname + '/'));
+app.use(express.json());
+//app.use(cookieParser());
+app.use(session({
+    secret: 'pene',
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(routes);
-app.use(bodyParser.json());
 
 //////////routes//////////
-app.post('/registrar', (req, res) => {
+app.post('/registro', async (req, res) => {
     //console.log(req.body);
-    Usuario.findOne({ nombre: req.body.nombre })
-    .then(yaHayAlguien => {
-        if(yaHayAlguien) res.send({r: false, msg: 'El nombre de usuario ya existe.'});
-        else new Usuario(req.body).save().then(data => res.send({r: true, msg: 'Registro exitoso.'}));
-    })
+    if(await Usuario.findOne({ nombre: req.body.nombre })) res.send('1');
+    else if(await Usuario.findOne({ correo: req.body.correo })) res.send('2');
+    else{
+        req.body.img = 'public/img/persona.png';
+        req.body.coins = 50;
+        req.body.exp = 0;
+        req.body.active = false;
+        req.session.user = await new Usuario(req.body).save();
+        res.send('3')
+    }
+});
+
+app.post('/login', async (req, res) => {
+    //console.log(req.body);
+    var user = await Usuario.findOne({ nombre: req.body.nombre });
+    if(user){
+        if(user.clave == req.body.clave){
+            req.session.user = user;
+            res.send('3');
+        }else res.send('2');
+    }else res.send('1');
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('Server funcionando'));
--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('index'));
-router.get('/login', (req, res) => res.render('login'));
-router.get('/registro', (req, res) => res.render('registro'));
-router.get('*', (req, res) => res.render('nada'));
+router.get('/', (req, res) => res.render('index', { user: req.session.user }));
+router.get('/login', (req, res) => res.render('login', { user: req.session.user }));
+router.get('/registro', (req, res) => res.render('registro', { user: req.session.user }));
+router.get('*', (req, res) => res.render('nada', { user: req.session.user }));
 
 module.exports = router;
